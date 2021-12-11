e8a70edc15754984621570e9a898b8dd7506cccd
António Wu <81578829+30001546@users.noreply.github.com>
2021-12-11 16:37:23 +0000

    v1
    1st version

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express"); // Carrega o framework Express
+const app = express(); // Construtor que inicializa uma aplicação Express
+
+app.use(express.json()); // Faz o parse (validação e interpretação) de solicitações do tipo application/json
+app.use(express.urlencoded({ extended: true })); // Faz o parse do conteúdo tipo application/x-www-form-urlencoded
+require("./routes/rotas")(app);
+const PORTA = process.env.PORT || 9090; // Estabelece a porta do servidor
+app.listen(PORTA, () => {
+  console.log(`O servidor está a ouvir na porta ${PORTA}`);
+});
+app.use(express.static("public"));
